5b670068e5e2e96bed534e9d28410ffdfd2293c0
chependuk16 <chependuk16@gmail.com>
2023-08-15 13:10:47 +0300

    Add logic for app

--- ColorizedApp-new/ViewController.swift
+++ ColorizedApp-new/ViewController.swift
@@ -9,11 +9,49 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: - IBOutlets
+    @IBOutlet var colorView: UIView!
+    
+    @IBOutlet var redValue: UILabel!
+    @IBOutlet var greenValue: UILabel!
+    @IBOutlet var blueValue: UILabel!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        redValue.text = String(format: "%.2f", redSlider.value)
+        greenValue.text = String(format: "%.2f", greenSlider.value)
+        blueValue.text = String(format: "%.2f", blueSlider.value)
+        
+        colorView.layer.cornerRadius = 10
+        colorViewChange()
     }
 
-
+    // MARK: - IBActions
+    @IBAction func redSliderAction() {
+        redValue.text = String(format: "%.2f", redSlider.value)
+        colorViewChange()
+    }
+    
+    
+    @IBAction func greenSliderAction() {
+        greenValue.text = String(format: "%.2f", greenSlider.value)
+        colorViewChange()
+    }
+    
+    @IBAction func blueSliderAction() {
+        blueValue.text = String(format: "%.2f", blueSlider.value)
+        colorViewChange()
+    }
+    
+    // MARK: - Private Methods
+    private func colorViewChange() {
+        colorView.backgroundColor = UIColor(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1)
+    }
+    
+    
 }
 
